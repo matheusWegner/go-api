854b67bac37c5e90ac9a748d01ca5347ad3d5cd3
Matheus <math.christo@gmail.com>
2023-12-17 09:11:18 -0300

    ajuste

--- api.go
+++ api.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-    "strconv"
-	"github.com/gorilla/mux"
 	"os"
+	"strconv"
 	"time"
+
 	jwt "github.com/golang-jwt/jwt/v4"
+	"github.com/gorilla/mux"
 )
 
 type APIServer struct {
@@ -28,7 +29,7 @@ func (s *APIServer) run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/user", makeHTTPhandleFunc(s.handleUser))
-	router.HandleFunc("/user/{id}",withJWTAuth(makeHTTPhandleFunc(s.handleGetUserById)))
+	router.HandleFunc("/user/{id}", withJWTAuth(makeHTTPhandleFunc(s.handleGetUserById)))
 
 	log.Println("JSON API server running on port ", s.listenAddr)
 	http.ListenAndServe(s.listenAddr, router)
@@ -46,12 +47,12 @@ func (s *APIServer) handleUser(w http.ResponseWriter, r *http.Request) error {
 }
 func (s *APIServer) handleGetUserById(w http.ResponseWriter, r *http.Request) error {
 	if r.Method == "GET" {
-    	id,err := getID(r)
+		id, err := getID(r)
 		if err != nil {
 			return err
 		}
-		user , err := s.store.getUserById(id)
-		
+		user, err := s.store.getUserById(id)
+
 		if err != nil {
 			return err
 		}
@@ -59,16 +60,16 @@ func (s *APIServer) handleGetUserById(w http.ResponseWriter, r *http.Request) er
 	}
 
 	if r.Method == "DELETE" {
-		return s.handleDeleteUser(w,r)
+		return s.handleDeleteUser(w, r)
 	}
 
-	return fmt.Errorf("method not allowed %s" , r.Method)
+	return fmt.Errorf("method not allowed %s", r.Method)
 
 }
 
 func (s *APIServer) handleGetUser(w http.ResponseWriter, r *http.Request) error {
-    users , err := s.store.getUsers()
-	
+	users, err := s.store.getUsers()
+
 	if err != nil {
 		return err
 	}
@@ -76,12 +77,12 @@ func (s *APIServer) handleGetUser(w http.ResponseWriter, r *http.Request) error 
 	return WriteJSON(w, http.StatusOK, users)
 }
 func (s *APIServer) handleCreateUser(w http.ResponseWriter, r *http.Request) error {
-    createUserRequest := new(CreateUserRequest)
+	createUserRequest := new(CreateUserRequest)
 	if err := json.NewDecoder(r.Body).Decode(&createUserRequest); err != nil {
-	   return err
+		return err
 	}
 
-    user := newUser(createUserRequest.Email,createUserRequest.UserName)
+	user := newUser(createUserRequest.Email, createUserRequest.UserName)
 
 	if err := s.store.createUser(user); err != nil {
 		return nil
@@ -93,10 +94,10 @@ func (s *APIServer) handleCreateUser(w http.ResponseWriter, r *http.Request) err
 	}
 
 	fmt.Println("JWT token : ", tokenString)
-	return WriteJSON(w,http.StatusOK,user)
+	return WriteJSON(w, http.StatusOK, user)
 }
 func (s *APIServer) handleDeleteUser(w http.ResponseWriter, r *http.Request) error {
-	id,err := getID(r)
+	id, err := getID(r)
 	if err != nil {
 		return err
 	}
@@ -104,7 +105,7 @@ func (s *APIServer) handleDeleteUser(w http.ResponseWriter, r *http.Request) err
 	if err := s.store.deleteUser(id); err != nil {
 		return err
 	}
-	return WriteJSON(w , http.StatusOK , map[string]int{"deleted":id})
+	return WriteJSON(w, http.StatusOK, map[string]int{"deleted": id})
 }
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
@@ -116,39 +117,49 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
-
 func withJWTAuth(hendlerFunc http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter , r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("calling JWT auth middleware")
 		tokenString := r.Header.Get("x-jwt-token")
-		_,err := validateJWT(tokenString)
+		token, err := validateJWT(tokenString)
 		if err != nil {
-			WriteJSON(w,http.StatusForbidden,ApiError{Error: "invalid token"})
-			return 
+			WriteJSON(w, http.StatusForbidden, ApiError{Error: "invalid token"})
+			return
 		}
-		hendlerFunc(w,r)
+
+		if !token.Valid {
+			WriteJSON(w, http.StatusForbidden, ApiError{Error: "invalid token"})
+			return
+		}
+
+		//        userID := getID(r);
+
+		claims := token.Claims.(jwt.MapClaims)
+		fmt.Println(claims)
+
+		hendlerFunc(w, r)
 	}
 }
 
-func createJWT (user *User) (string, error) {
+func createJWT(user *User) (string, error) {
 	claims := &jwt.MapClaims{
 		"ExpiresAt": jwt.NewNumericDate(time.Unix(1516239022, 0)),
-		"Issuer":  "go-api",
-		"userId": user.ID,
+		"Issuer":    "go-api",
+		"userId":    user.ID,
 	}
 	secret := os.Getenv("JWT_SECRET")
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	return token.SignedString([] byte(secret))
+	return token.SignedString([]byte(secret))
 }
 
-func validateJWT(tokenString string) (*jwt.Token,error){
+func validateJWT(tokenString string) (*jwt.Token, error) {
 	secret := os.Getenv("JWT_SECRET")
-	return jwt.Parse(tokenString , func(token *jwt.Token) (interface{} , error){
-		if _,ok := token.Method.(*jwt.SigningMethodHMAC); ! ok {
-			return nil , fmt.Errorf("Unexpected signinng method : %v" , token.Header["alg"])
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signinng method : %v", token.Header["alg"])
 		}
-		return []byte(secret) , nil
+		return []byte(secret), nil
 	})
 }
 
@@ -167,11 +178,11 @@ func makeHTTPhandleFunc(f apiFunc) http.HandlerFunc {
 	}
 }
 
-func getID(r *http.Request) (int , error){
+func getID(r *http.Request) (int, error) {
 	idStr := mux.Vars(r)["id"]
-    id,err := strconv.Atoi(idStr)
+	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		return id , fmt.Errorf("ivalid id given %s" , idStr)
+		return id, fmt.Errorf("ivalid id given %s", idStr)
 	}
-	return id , nil
+	return id, nil
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.23
 require (
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.2.2 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
